ed31f33f23ad7a5a6db8e0241b41e512393c8db4
Mat Crawshaw <mat_crawshaw@hotmail.com>
2023-01-30 17:38:55 +0000

    added basic structure and used moment to show date at the top

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Daily-Planner-App
+
+
+
+psuedo
+
+generate 8 divs with save buttons on the right 
+create event listeners for buttons to save input of text box 
---
+++ script.js
@@ -0,0 +1,20 @@
+let di
+
+let currentDay = $("#currentDay");
+
+var today = moment("3/11/20", "DD-MM-YY").format("dddd, MMMM D ");
+
+
+
+let save9 = $("#Save9");
+
+currentDay.text(today)
+
+
+
+
+save9.click(function (e) { 
+
+    console.log("ghelooodsad")
+    
+});
