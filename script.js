694566044cf514cbd09c7c4859ca61bad97627c6
matcrawshaw <mat_crawshaw@hotmail.com>
2023-03-19 19:27:50 +0000

    fixed bug repeating saved array on second reload

--- script.js
+++ script.js
@@ -4,36 +4,24 @@ let currentDay = $("#currentDay");
 let today = moment()
 var todayMain = moment().format("dddd, MMMM D ");
 
-
-
-let save9 = $("#Save9");
-
 currentDay.text(todayMain)
 
-
-
-
-
 let hourNum = moment().hour();
-
 
 // initial arrays that the elements are to be generated from 
 workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 workHoursText = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 const main = $("main");
-savedNotes = []
+savedNotes = [null, null, null, null, null, null, null, null, null]
 
 // loop to create rows with relevent classes, with titles and text areas, as well as save buttons.
 for (let i = 0; i < workHours.length; i++) {
 const row = $("<div>", {"class": "row"});
 const hourTitle = $("<h2>", {"class": "hour hourTitle"}).text(workHoursText[i]);
-const memoArea = $("<textarea>", {"class": "description"});
+const memoArea = $("<textarea>", {"class": "description"}).attr("data-index", workHours[i]);
 
 const saveBtnbBox = $("<div>", {"class": "saveBtn"})
 const saveBtn = $("<i>", {"class": "fas fa-save"});
-
-
-
 
 // appending save button to the save button box, and then the title, text areas, and button box to each row.
 saveBtnbBox.append(saveBtn)
@@ -47,26 +35,18 @@ let storedNotes = JSON.parse(localStorage.getItem("notes"));
 
 
 if (storedNotes !== null) {
-    savedNotes = savedNotes.concat(storedNotes)
+    savedNotes[i] = storedNotes[i]
 }
-
+    
     memoArea.text(savedNotes[i])
-
-
-
-
-
-
 
 /////// push new notes to array and store in local storage when save button clicked
 
 $(saveBtn).click(function (e) { 
 e.preventDefault()
 let savedNote = $(memoArea).val().trim();
-   
+
 savedNotes[i] = savedNote
-
-
 
 localStorage.setItem("notes", JSON.stringify(savedNotes));
 });
@@ -84,34 +64,6 @@ memoArea.addClass("future")
 }
 
 }
-
-
-memoArray = []
-
-
-/*
-for $("div").each(function (index, element) {
-    // element == this
-    if (hourNum > parseFloat(hour )) {
-$("div").addClass(past);
-    }
-});
-
-*/
-
-
-let area0 = $("#textarea0")
-let save0  = $
-
-save9.click(function (e) 
-
-
-{ 
-
-    console.log("ghelooodsad")
-    
-});
-
 
 
 
